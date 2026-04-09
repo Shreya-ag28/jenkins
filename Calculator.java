81b76944bfba06bb08c707bfd00ebef0f6217e53
Shreya-ag28 <shreyapopular@gmail.com>
2026-04-09 10:26:56 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,40 @@
 
+public class Calculator {
+    public static void main(String[] args) {
+        
+        double num1 = 10;   
+        double num2 = 5;    
+        char operator = '+'; 
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
